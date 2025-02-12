4c845b8c32c277bd70de41da018f663399a52d12
Pranav Sodani <sodanipranav2004@gmail.com>
2025-02-12 12:21:15 +0530

    update the header and make the profile page private

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
     },
     avatar:{
         type: String,
-        default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+        default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm3RFDZM21teuCMFYx_AROjt-AzUwDBROFww&s"
       },
 },{timestamps: true}); 
 

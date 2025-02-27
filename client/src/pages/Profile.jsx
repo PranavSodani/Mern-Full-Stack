5372aa00308b635410362c20f029af1668d6cec7
Pranav Sodani <sodanipranav2004@gmail.com>
2025-02-28 01:08:59 +0530

    add delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import { useState } from "react";
-import {updateUserStart,updateUserSuccess,updateUserFailure} from '../redux/user/userSlice';
+import {updateUserStart,updateUserSuccess,updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess} from '../redux/user/userSlice';
 import { useDispatch } from "react-redux";
 export default function Profile() {
   const {currentUser, loading, error } = useSelector((state) => state.user);
@@ -35,6 +35,23 @@ export default function Profile() {
       dispatch(updateUserFailure(error.message))
     }
   }
+  const handleDeleteUser = async() =>{
+    try{
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`,{
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if(data.success === false){
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data)); 
+    }
+    catch(error){
+      dispatch(deleteUserFailure(error.message));
+    }
+  }
   return (
     <div className="p-3 mx-auto max-w-lg">
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -46,7 +63,7 @@ export default function Profile() {
         <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">update</button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Delete account</span>
+        <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete account</span>
         <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
       <p className="text-red-700 mt-5">{error ? error : ''}</p>

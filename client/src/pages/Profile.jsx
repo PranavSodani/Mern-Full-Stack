aafa20b0bceecfd4d3cc34451180d2f00bb65b6d
Pranav Sodani <sodanipranav2004@gmail.com>
2025-03-01 16:32:34 +0530

    complete create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,89 +1,134 @@
 import { useSelector } from "react-redux";
 import { useState } from "react";
-import {updateUserStart,updateUserSuccess,updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart} from '../redux/user/userSlice';
+import { Link } from "react-router-dom";
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  signOutUserStart,
+} from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 
 export default function Profile() {
-  const {currentUser, loading, error } = useSelector((state) => state.user);
-  const [formData,setFormData] = useState({});
-  const [updateSuccess,setUpdateSuccess] = useState(false);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
+  const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
   const dispatch = useDispatch();
 
   const handleChange = (e) => {
-    setFormData({...formData,[e.target.id] : e.target.value});
-  }
-  const handleSubmit = async (e) => { 
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    try{
+    try {
       dispatch(updateUserStart());
-      const res = await fetch(`/api/user/update/${currentUser._id}`,{
-        method:'POST',
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
-
       });
       const data = await res.json();
-      if(data.success === false){
+      if (data.success === false) {
         dispatch(updateUserFailure(data.message));
         return;
       }
       dispatch(updateUserSuccess(data));
       setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
     }
-    catch(error){
-      dispatch(updateUserFailure(error.message))
-    }
-  }
-  const handleDeleteUser = async() =>{
-    try{
+  };
+  const handleDeleteUser = async () => {
+    try {
       dispatch(deleteUserStart());
-      const res = await fetch(`/api/user/delete/${currentUser._id}`,{
-        method: 'DELETE',
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: "DELETE",
       });
       const data = await res.json();
-      if(data.success === false){
-        dispatch(deleteUserFailure(data.message));
-        return;
-      }
-      dispatch(deleteUserSuccess(data)); 
-    }
-    catch(error){
-      dispatch(deleteUserFailure(error.message));
-    }
-  }
-  const handleSignOut = async () => {
-    try{
-      dispatch(signOutUserStart());
-      const res = await fetch('/api/auth/signout');
-      const data = await res.json();
-      if(data.success === false){
+      if (data.success === false) {
         dispatch(deleteUserFailure(data.message));
         return;
       }
       dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
     }
-    catch(error){
+  };
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch("/api/auth/signout");
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
       dispatch(deleteUserFailure(data.message));
     }
-  }
+  };
   return (
     <div className="p-3 mx-auto max-w-lg">
-      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <img className="h-24 w-24 rounded-full object-cover cursor-pointer self-center mt-2" src={currentUser.avatar} alt="profile" onError={(e) => e.target.src = '/images/image.png'} />
-        <input type="text" onChange={handleChange} placeholder="username"  defaultValue={currentUser.username}  className="border p-3 rounded-lg" id="username"/>
-        <input type="email"  onChange={handleChange}placeholder="email" defaultValue={currentUser.email}className="border p-3 rounded-lg" id="email"/>
-        <input type="password"  placeholder="password"  onChange={handleChange}className="border p-3 rounded-lg" id="password"/>
-        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">update</button>
+        <img
+          className="h-24 w-24 rounded-full object-cover cursor-pointer self-center mt-2"
+          src={currentUser.avatar}
+          alt="profile"
+          onError={(e) => (e.target.src = "/images/image.png")}
+        />
+        <input
+          type="text"
+          onChange={handleChange}
+          placeholder="username"
+          defaultValue={currentUser.username}
+          className="border p-3 rounded-lg"
+          id="username"
+        />
+        <input
+          type="email"
+          onChange={handleChange}
+          placeholder="email"
+          defaultValue={currentUser.email}
+          className="border p-3 rounded-lg"
+          id="email"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={handleChange}
+          className="border p-3 rounded-lg"
+          id="password"
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..," : "Update"}
+        </button>
+        <Link className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to={"/create-listing"}>Create Listing</Link>
       </form>
       <div className="flex justify-between mt-5">
-        <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete account</span>
-        <span onClick={handleSignOut } className="text-red-700 cursor-pointer">Sign out</span>
+        <span
+          onClick={handleDeleteUser}
+          className="text-red-700 cursor-pointer"
+        >
+          Delete account
+        </span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">
+          Sign out
+        </span>
       </div>
-      <p className="text-red-700 mt-5">{error ? error : ''}</p>
-      <p className="text-green-700 mt-5">{updateSuccess ? 'User is updated successfully' : ''}</p>
+      <p className="text-red-700 mt-5">{error ? error : ""}</p>
+      <p className="text-green-700 mt-5">
+        {updateSuccess ? "User is updated successfully" : ""}
+      </p>
     </div>
-  )
+  );
 }
